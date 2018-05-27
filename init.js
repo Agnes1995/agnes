634f73925504acd0d131cfb7699cc7de2d7db205
mheere <heeremans.marcel@gmail.com>
2018-05-27 21:02:18 +0100

    updates 2

--- init.js
+++ init.js
@@ -1,4 +1,7 @@
 //import * as $ from 'jquery'
+//import add5 from './utils'
+//var add5 = require('./utils');
+const { add5 } = require('./utils');
 
 let x = 5;
 
@@ -7,3 +10,5 @@ let calc = x => x * 5;
 console.log(calc(x));
 
 // and this on mymac branch
+
+console.log("add5: " + add5(5));
